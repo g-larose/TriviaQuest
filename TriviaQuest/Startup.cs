efe8aff82cc7d1d7cb670ab608bb2e22ae435d15
g-larose <larose.g@fromtier.com>
2024-02-21 03:01:44 -0500

    Add project files.

---
+++ TriviaQuest/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaQuest
+{
+    public class Startup
+    {
+        private readonly IHost _host;
+
+        public Startup()
+        {
+            _host = Host.CreateDefaultBuilder().ConfigureServices(services =>
+            {
+                
+            }).Build();
+        }
+    }
+}
